286175940600f61fc40e54f3486dfba291182985
Arun Biradar <arunarb98@gmail.com>
2024-09-01 20:54:21 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 
-logger.info("Welcome to our custom file")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+except Exception as e:
+
+    logger.exception(e)
+    raise e
