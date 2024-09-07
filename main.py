37a0383f0278ee2652b6db976a6a65d98671dd0d
Arun Biradar <arunarb98@gmail.com>
2024-09-07 18:42:13 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
+except Exception as e:
+
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = DataIngestionTrainingPipline()
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-
+from mlProject.utils.common import read_yaml, create_directories
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -8,3 +8,26 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir:Path
+    all_schema: dict
+
+
+def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
