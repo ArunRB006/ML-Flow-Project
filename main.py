6d4ef452a0aaaed90862c0aac98cce04129e48be
Arun Biradar <arunarb98@gmail.com>
2024-09-07 19:57:18 +0530

    Data Validation new change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ except Exception as e:
 STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
-    obj = DataIngestionTrainingPipline()
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from pathlib import Path
-from mlProject.utils.common import read_yaml, create_directories
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -15,19 +14,3 @@ class DataValidationConfig:
     STATUS_FILE: str
     unzip_data_dir:Path
     all_schema: dict
-
-
-def get_data_validation_config(self) -> DataValidationConfig:
-        config = self.config.data_validation
-        schema = self.schema.COLUMNS
-
-        create_directories([config.root_dir])
-
-        data_validation_config = DataValidationConfig(
-            root_dir=config.root_dir,
-            STATUS_FILE=config.STATUS_FILE,
-            unzip_data_dir = config.unzip_data_dir,
-            all_schema=schema,
-        )
-
-        return data_validation_config
